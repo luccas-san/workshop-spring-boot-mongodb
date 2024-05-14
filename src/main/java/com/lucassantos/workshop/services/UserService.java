29b6b1c885f4380e800c20b31c5d926dc76c00b8
luccas-san <lucas.sampa095@hotmail.com>
2024-05-13 21:21:41 -0300

    Insercao de usuario com POST

--- src/main/java/com/lucassantos/workshop/services/UserService.java
+++ src/main/java/com/lucassantos/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucassantos.workshop.domain.User;
+import com.lucassantos.workshop.dto.UserDTO;
 import com.lucassantos.workshop.repository.UserRepository;
 import com.lucassantos.workshop.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
